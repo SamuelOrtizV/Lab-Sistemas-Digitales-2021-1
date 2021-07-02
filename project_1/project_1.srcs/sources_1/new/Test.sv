dd3873dd33787c63ecf195a15f3ee7035e18da36
SamuelOrtizV <samuel.ortizv@usm.cl>
2021-07-02 16:25:10 -0400

    test
    test

---
+++ project_1/project_1.srcs/sources_1/new/Test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.07.2021 16:23:17
+// Design Name: 
+// Module Name: Test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//esp
+module Test(
+
+    );
+endmodule
